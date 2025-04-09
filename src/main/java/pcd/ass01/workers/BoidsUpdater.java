3b301bca341e6d4838507674e19356e96081c52a
MatteoZacca <matteo.zaccarini4@studio.unibo.it>
2025-04-09 10:02:44 +0200

    Implementazione BoidsUpdater e pulizia codice

--- src/main/java/pcd/ass01/workers/BoidsUpdater.java
+++ src/main/java/pcd/ass01/workers/BoidsUpdater.java
@@ -1,56 +1,57 @@
 package pcd.ass01.workers;
 
 import pcd.ass01.BoidsModel;
-import pcd.ass01.synchronizers.MyCyclicBarrier;
+import pcd.ass01.Boid;
 
 import java.util.ArrayList;
-import java.util.List;
+
 
 public class BoidsUpdater {
 
-    private final int availableProcessors = Runtime.getRuntime().availableProcessors();
-
-    private final List<Agent> agents = new ArrayList<>();
-
-    private final MyCyclicBarrier startUpdateBarrier;
-    private final MyCyclicBarrier readingDoneBarrier;
-    private final MyCyclicBarrier endUpdateBarrier;
-
     public BoidsUpdater() {
-
-        this.startUpdateBarrier = new MyCyclicBarrier(availableProcessors + 1);
-        this.readingDoneBarrier = new MyCyclicBarrier(availableProcessors);
-        this.endUpdateBarrier = new MyCyclicBarrier(availableProcessors + 1);
-
-
-        for (int index = 0; index < availableProcessors; index++) {
-            Agent agent = new Agent(index, this.startUpdateBarrier, this.endUpdateBarrier, this.readingDoneBarrier);
-            agents.add(agent);
-            agent.start();
-        }
     }
 
     public void update(BoidsModel model) {
-        this.agents.forEach(boid -> boid.setModel(model));
+        ArrayList<Thread> readers = new ArrayList<Thread>();
 
-        try {
-            this.startUpdateBarrier.await(); // Quando viene premuto il tasto 'Play'
-            // nella GUI, si entra nel primo ciclo if all'interno del metodo runSimulation.
-            // Una volta che si entra nell'if, viene chiamato il metodo 'update', e una
-            // volta eseguita l'istruzione di cui sopra, anche il main raggiunge
-            // 'startUpdateBarrier', dove gli altri thread sono già stati soggetti alla
-            // 'wait()', ed essendo il main l'ultimo thread necessario per fare crollare
-            // la barriera, una volta arrivato, 'startUpdateBarrier' crolla.
-        } catch (InterruptedException ex) {
-            throw new RuntimeException(ex);
+        for (int i = 0; i < model.getBoids().size(); i++) {
+            Boid boid = model.getBoids().get(i);
+            Thread agent = Thread.ofVirtual().unstarted(() -> {
+                boid.readNearbyBoids(model);
+            });
+            agent.start();
+
+            readers.add(agent);
         }
 
-        try {
-            this.endUpdateBarrier.await(); // Analogia con il ragionamento sopra sulla
-            // 'startUpdatebarrier'
-        } catch (InterruptedException ex) {
-            throw new RuntimeException(ex);
+        readers.forEach(it -> {
+            try {
+                it.join();
+            } catch (InterruptedException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        ArrayList<Thread> writers = new ArrayList<Thread>();
+
+        for (int i = 0; i < model.getBoids().size(); i++) {
+            Boid boid = model.getBoids().get(i);
+            Thread agent = Thread.ofVirtual().unstarted(() -> {
+                boid.updateVelocity(model);
+                boid.updatePos(model);
+            });
+            agent.start();
+
+            writers.add(agent);
         }
+
+        writers.forEach(it -> {
+            try {
+                it.join();
+            } catch (InterruptedException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
     }
 
 }
